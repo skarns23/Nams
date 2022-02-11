bb10f234589fbf750a2188e9d584c07b97623696
skarns23 <skarns23@naver.com>
2022-02-11 17:41:21 +0900

    hash leve2

--- programmers/Level1/src/hash/PhoneNumber.java
+++ programmers/Level1/src/hash/PhoneNumber.java
@@ -5,12 +5,12 @@ public class PhoneNumber {
 	static boolean solution(String[] phone_book) {
 		boolean answer = true;
 		Arrays.sort(phone_book);
-		for(int i =0 ; i < phone_book.length;i++) {
-			if(phone_book[i+1].contains(phone_book[i])) {
+		for(int i = 0 ; i <phone_book.length-1;i++) {
+			if(phone_book[i+1].startsWith(phone_book[i]))
 				return false;
-			}
 		}
-		return true;
+		return answer;
+		
 	}
 	public static void main(String[] args) {
 		String[] arr = {"119","97674223","1195524421"};
