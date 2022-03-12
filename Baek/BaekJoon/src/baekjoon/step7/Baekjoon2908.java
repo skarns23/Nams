b490c465a60d459c66bb59a634341457d5ba033d
skarns23 <skarns23@naver.com>
2022-03-12 21:24:57 +0900

    백준 자바 풀이

--- Baek/BaekJoon/src/baekjoon/step7/Baekjoon2908.java
+++ Baek/BaekJoon/src/baekjoon/step7/Baekjoon2908.java
@@ -7,6 +7,7 @@ public class Baekjoon2908 {
 		Scanner sc  =new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
+		
 		a = a%10*100 +(a/10%10)*10 + a/100;
 		b = b%10*100 +(b/10%10)*10 + b/100;
 		if( a>b) {
