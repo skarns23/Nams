09fd6929471fc1b519a14cca723a32a63866f8d4
skarns23 <skarns23@naver.com>
2022-03-19 23:49:14 +0900

    자바공부

---
+++ Baek/BaekJoon/src/baekjoon/step14/Baekjoon15649.java
@@ -0,0 +1,16 @@
+package baekjoon.step14;
+import java.io.*;
+import java.util.*;
+public class Baekjoon15649 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw  =new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int num = Integer.parseInt(st.nextToken());
+		int choice = Integer.parseInt(st.nextToken());
+		
+		
+	}
+
+}
