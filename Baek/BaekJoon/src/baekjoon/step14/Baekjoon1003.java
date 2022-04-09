862f69255430fe7aafc1a240b335a88b6a989d15
skarns23 <skarns23@naver.com>
2022-04-09 21:14:47 +0900

    1003번 문제 해결

--- Baek/BaekJoon/src/baekjoon/step14/Baekjoon1003.java
+++ Baek/BaekJoon/src/baekjoon/step14/Baekjoon1003.java
@@ -3,8 +3,12 @@ import java.io.*;
 import java.util.*;
 public class Baekjoon1003 {
 	
-	
-	public static void fibo(int depth) {
+	public static int[] arr = new int [41];
+	public static void fibo() {
+		
+		for(int i =2;i<=40;i++) {
+			arr[i] = arr[i-1] + arr[i-2];
+		}
 		
 	}
 	public static void main(String[] args)  throws IOException{
@@ -12,9 +16,17 @@ public class Baekjoon1003 {
 		BufferedReader br  =new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int size = Integer.parseInt(br.readLine());
+		arr[1] = 1;
+		fibo();
 		for(int i = 0 ; i<size;i++) {
 			int num = Integer.parseInt(br.readLine());
+			if(num == 0 ) {
+				bw.write("1 0\n");
+			}else {
+				bw.write(arr[num-1]+" "+arr[num]+"\n");
+			}
 		}
+		bw.flush();
 		
 		
 	}
