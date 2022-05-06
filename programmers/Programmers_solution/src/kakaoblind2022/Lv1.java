9481ba2ba69285cfdfd4255b90a9fc4985712f4d
skarns23 <skarns23@naver.com>
2022-05-06 23:34:45 +0900

    프로그래머스 신고결과받기, 행렬 테두리 회전하기 해결

--- programmers/Programmers_solution/src/kakaoblind2022/Lv1.java
+++ programmers/Programmers_solution/src/kakaoblind2022/Lv1.java
@@ -2,43 +2,41 @@
 package kakaoblind2022;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.*;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 public class Lv1 {
 	public static  int[] solution(String[] id_list, String[] report, int k) {
 		int[] answer = new int [id_list.length];
-		int[] result = new int [id_list.length];
-		Map<String,String> map = new HashMap<String, String>();
-		HashMap<String,Integer> count_value = new HashMap<String,Integer>();
-		for(String s :id_list)
-			count_value.put(s, 0);
-		for(String s :report) {
+		HashMap<String,HashSet<String>> map = new HashMap<>();
+		//report 결과를 담을 map
+		HashMap<String,Integer> report_count = new HashMap<String, Integer>();
+		for(String s :id_list) {
+			map.put(s, new HashSet<>()); //id_list와 초기설정
+			report_count.put(s, 0); //메일 수를 받을 report_count map을 초기화 
+		}
+		for(String s : report) {
 			String[] str = s.split(" ");
-			if(map.containsKey(str[1])) {
-				if(map.get(str[1]).contains(str[0]))
-					continue;
-				String x = map.get(str[1]) +" "+str[0];
-				map.put(str[1], x);
-			}
-			else 
-			map.put(str[1], str[0]);
-		}
-		for(int i= 0; i<id_list.length;i++) {
-			if(map.get(id_list[i])!=null) {
-			String[] str = map.get(id_list[i]).split(" ");
-			if(str.length>=k) {
-				for(int z = 0; z<str.length;z++)
-					count_value.put(str[z],count_value.get(str[z])+1);
-			}
-			}
+			map.get(str[1]).add(str[0]); // 피신고자를 key로 신고자를 중복을 허용하지않는 HashSet에 넣어 줌 
 		}
 		
-		for(int x = 0 ; x<id_list.length;x++) {
-			answer[x] =count_value.get(id_list[x]);
+		for(String s : id_list) {
+			if(map.get(s).size()>=k) {
+				for(String name : map.get(s)) {
+					report_count.put(name, report_count.get(name)+1);
+				}
+			}
 		}
-		
-			return answer;
+		int i =0;
+		for(String s :id_list) {
+			if(report_count.get(s)!=0) {
+			answer[i] = report_count.get(s);
+			}
+			i++;
+		}
+		return answer;
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -46,7 +44,7 @@ public class Lv1 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int k = 2;
 		String[] str = {"muzi", "frodo", "apeach", "neo"};
-		String[] report = {"frodo neo","muzi neo","apeach frodo","muzi frodo","apeach muzi"};
+		String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
 		int[] result = solution(str,report,k);
 		for(int i :result)
 			System.out.println(i);
