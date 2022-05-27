417688a79f6b699cfab20834caf74d3f577ca81a
skarns23 <skarns23@naver.com>
2022-05-28 00:19:01 +0900

    oucur error

--- Baek/BaekJoon/src/baekjoon/step14/Baekjoon1149.java
+++ Baek/BaekJoon/src/baekjoon/step14/Baekjoon1149.java
@@ -1,10 +1,18 @@
 package baekjoon.step14;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Baekjoon1149 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	static int [][]cost;
+	static int num;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
 	}
 
 }
---
+++ programmers/Programmers_solution/src/greedy/Lv2_Stick.java
@@ -0,0 +1,20 @@
+package greedy;
+
+public class Lv2_Stick {
+	class Solution{
+		public int solution(String name) {
+			int answer= 0;
+			String str ="";
+			for(int i = 0;i<name.length();i++) {
+				str +="A";
+			}
+			int count;
+			int pos = 0;
+			while(!str.equals(name)) {
+				if(str.charAt(pos)!=name.charAt(pos)) {
+				}
+			}
+			return answer; 
+		}
+	}
+}
