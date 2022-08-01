0cea1b9a857ea83f617336417720f00336cb9a73
skarns23 <skarns23@naver.com>
2022-08-01 23:57:11 +0900

    stack queue 풀이

---
+++ Programmers/Progerammers/src/queue/Bracket.java
@@ -0,0 +1,26 @@
+package queue;
+
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for(int i =0 ; i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='('){
+                stack.push(c);
+            }else if (stack.isEmpty()){
+                answer= false;
+                return answer;
+            }else{
+                stack.pop();
+            }
+        }
+        if(stack.isEmpty()){
+            return answer;
+        }else {
+
+        }
+        return false;
+    }
+}
